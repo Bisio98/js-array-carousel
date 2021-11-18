5ceba8abf8f6358b231ac51fa480173e1782973c
Bisio98 <alberto.bisetti@hotmail.it>
2021-11-18 16:46:43 +0100

    milestone 1.5

---
+++ milestone-2/js/script.js
@@ -0,0 +1,49 @@
+const imgArray = [
+    'img/01.jpg',
+    'img/02.jpg',
+    'img/03.jpg',
+    'img/04.jpg',
+    'img/05.jpg'
+];
+
+const title = [
+    'Svezia',
+    'Svizzera',
+    'Gran Bretagna',
+    'Germania',
+    'Paradise'
+]
+
+const text = [
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    'Lorem ipsum',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+]
+
+
+const imgContainer = document.querySelector('.big_image');
+const descriptionContainer = document.querySelector('.description')
+const imgList = document.querySelector('.list_image')
+
+for( let i = 0; i < imgArray.length; i++ ) {
+    const thisPath = imgArray[i];
+    const newImg = `<div class="hide"><img src="${thisPath}" alt="Img ${i}"></div>`;
+
+    imgContainer.innerHTML += newImg;
+
+    const newDescrition = `<div class="hide"><img src="${thisPath}" alt="Img ${i}"></div>`;
+
+    imgContainer.innerHTML += newImg;
+
+    const newLittleImg = `<li><img class="element" src="${thisPath}" alt="Img ${i}"></li>`; 
+
+    imgList.innerHTML += newLittleImg;
+}
+
+let onImage = 0;
+const bigPhoto = document.getElementsByClassName('hide');
+const smallPhoto = document.getElementsByClassName('element');
+bigPhoto[onImage].classList.add('show');
+smallPhoto[onImage].classList.add('active');
